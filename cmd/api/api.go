d08a33770cf0c18109f00188083722739d72a7b7
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2025-03-01 00:17:51 -0500

    run() and mount() defined on ServeMux

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 type application struct {
@@ -13,12 +14,22 @@ type config struct {
 	addr string
 }
 
-func (app *application) run() error {
+func (app *application) mount() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+
 	srv := http.Server{
-		Addr:    app.config.addr,
-		Handler: mux,
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
 	}
 
 	log.Printf("server started on %s", app.config.addr)
