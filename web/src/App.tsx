f25113a49c23f91ec7138286dc5a53857e1a9701
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2025-04-17 15:33:56 -0400

    Consuming Go APIs using react frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 
-import { Button} from '@chakra-ui/react'
+import './App.css'
+
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/v1"
+
 function App() {
   return (
     <>
-      <Button> Hello</Button>
+      <div>App home screen</div>
     </>
   )
 }
