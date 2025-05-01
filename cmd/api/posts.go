5e11de87d7bbcd44611fca78adf5ffd16d66ea56
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2025-04-30 23:12:03 -0400

    Adding posts handler

---
+++ cmd/api/posts.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+type postKey string
+
+const postCtx postKey = "post"
+
+type CreatePostPayload struct {
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
